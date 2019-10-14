d98fd2b65b94cfde4387a1c9ee954910be4ea01d
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2019-10-14 21:16:50 +0200

    second

---
+++ chapter 3/3.2/text_sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  vector<string> text;
+
+  auto beg = text.begin(), end = text.end();
+  auto mid = text.begin() + (end - beg)/2;
+
+  while (mid != end && *mid != sought)
+
+  return 0;
+}
